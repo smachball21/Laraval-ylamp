00c5e07784848327bf49ad954ba9b041ba6b345d
kayreddine.amama@saint-benigne.fr <kayreddine.amama@saint-benigne.fr>
2020-11-10 11:24:38 +0100

    Fix comments migration

--- database/migrations/2020_11_10_094216_create_comments_table.php
+++ database/migrations/2020_11_10_094216_create_comments_table.php
@@ -13,7 +13,7 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::table('comments', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->text('comment');
             $table->bigInteger('author_id')->unsigned();
